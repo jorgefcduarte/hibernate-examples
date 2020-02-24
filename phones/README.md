dcca9f8822a1b306c1512a34074fef9d78d88dc2
Jorge Duarte <jorgefcduarte@gmail.com>
2020-02-23 23:51:52 -0300

    Initial commit of an hibernate example which uses Criteria's Restriction.like
    Answer to a stackoverflow question:
    
    https://stackoverflow.com/questions/60341986/how-to-use-liststring-params-in-restrictions-like/

---
+++ phones/README.md
@@ -0,0 +1,3 @@
+# Hibernate example using Restrictions.like using a List of parameters
+
+This is a simple project which show a workaround of an hibernate criteria query which receives a list of phone numbers then are used in a Restrictions.like and a Restrictions.or.
---
+++ phones/src/main/java/dev/intranet/exampleapp/controllers/PhonesController.java
@@ -0,0 +1,75 @@
+package dev.intranet.exampleapp.controllers;
+
+import dev.intranet.exampleapp.application.domain.Phone;
+import dev.intranet.exampleapp.application.domain.repositories.PhoneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/phones")
+public class PhonesController {
+
+    @Autowired
+    private PhoneRepository phoneRepository;
+
+    public PhonesController() {
+
+    }
+
+    @PostConstruct
+    private void feedPhones() {
+
+        long countRegisteredPhones = phoneRepository.countPhones();
+
+        if (phoneRepository.countPhones() == 0) {
+            Phone p1 = new Phone("(11)9898-1234");
+            Phone p2 = new Phone("(11)9898-4321");
+            Phone p3 = new Phone("(21)1234-7654");
+            Phone p4 = new Phone("(21)9899-8787");
+            Phone p5 = new Phone("(81)6565-1234");
+            Phone p6 = new Phone("(11)7378-8777");
+
+            phoneRepository.save(p1);
+            phoneRepository.save(p2);
+            phoneRepository.save(p3);
+            phoneRepository.save(p4);
+            phoneRepository.save(p5);
+            phoneRepository.save(p6);
+        }
+
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> all(){
+
+        try {
+
+            Set<String> numbersToFind = new HashSet<>(Arrays.asList("(81)6565-1234", "(11)7378-8777"));
+
+            List<Phone> phones = phoneRepository.byNumbers(numbersToFind);
+            return ResponseEntity.ok().body(phones);
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().body("There was an error searching the phones.");
+        }
+
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> add(Phone phone) {
+        try {
+            return ResponseEntity.ok(phoneRepository.save(phone));
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().body("There was an error saving the phone.");
+        }
+    }
+
+}
